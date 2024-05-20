a2a72b176ce6624ed9323c42bf7fdc6b64d757a4
FeAranha <felipearanha.c@gmail.com>
2024-05-20 16:38:38 -0300

    change img for Image 'next/image'

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,19 @@
 import { ActiveLink } from '../ActiveLink'
-
 import { SignInButton } from '../SignInButton'
 
 import styles from './styles.module.scss'
+import Image from 'next/image'
 
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
+                <Image src="/images/logo.svg" alt="ig.news" width={68} height={68}/>
                 <nav>
                     <ActiveLink activeClassName={styles.active} href="/">
                     <a>Home</a>
                     </ActiveLink>
-                    <ActiveLink  activeClassName={styles.active} href="/posts" prefetch>
+                    <ActiveLink  activeClassName={styles.active} href="/posts">
                     <a>Posts</a>
                     </ActiveLink>
                 </nav>
