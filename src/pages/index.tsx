ddab193d51cde945849fed1a3d2d4efe94622889
FeAranha <felipearanha.c@gmail.com>
2024-05-22 19:16:22 -0300

    fix image w|h

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home({ product }:HomeProps) {
           </p>
           <SubscribeButton priceId={product.priceId}/>
         </section>
-        <Image src="/images/avatar.svg" alt="Girl coding" />
+        <Image src="/images/avatar.svg" alt="Girl coding" width={480} height={300}/>
       </main>
     </>
   )
